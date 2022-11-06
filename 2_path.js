a34c6cab88cf28f3698d8e70485048ce0084e39a
Muhammad Zain <95617382+zyn10@users.noreply.github.com>
2022-11-06 10:52:57 +0500

    Add files via upload

---
+++ 2_path.js
@@ -0,0 +1,4 @@
+const path=require('path')
+path.posix.basename('/tmp/myfile.html');
+
+console.log(path.extname(__filename));//--->gives extention
---
+++ 3_Os.js
@@ -0,0 +1,8 @@
+const os=require('os');
+console.log(os.freemem());
+console.log(os.version());
+console.log(os.hostname());
+console.log(os.release());
+console.log(os.homedir());
+console.log(os.uptime());
+console.log(os.type());
